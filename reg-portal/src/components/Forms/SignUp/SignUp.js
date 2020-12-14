e7cdf115d262bef56013e98e0e17abd0c927432c
Shambhavi Sharma <60618802+ShambhaviSharma0110@users.noreply.github.com>
2020-12-14 17:48:38 +0530

    validation

--- reg-portal/src/components/Forms/SignUp/SignUp.js
+++ reg-portal/src/components/Forms/SignUp/SignUp.js
@@ -2,16 +2,110 @@ import React, { Component } from "react";
 import Background from "../../../hoc/Background/Background";
 import axios from "axios";
 
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/
+);
+const phoneRegex = RegExp(
+  /^(\+91)?(-)?\s*?(91)?\s*?(\d{3})-?\s*?(\d{3})-?\s*?(\d{4})*$/
+);
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  // validate form errors being empty
+  Object.values(formErrors).forEach((val) => {
+    val.length > 0 && (valid = false);
+  });
+
+  // validate the form was filled out
+  Object.values(rest).forEach((val) => {
+    val === null && (valid = false);
+  });
+
+  return valid;
+};
 export class SignUp extends Component {
-  state = {
-    firstPage: true,
-    name: "",
-    regno: "",
-    email: "",
-    phone: "",
-    yearofstudy: 1,
-    password: "",
-    github: "",
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: null,
+      regNo: null,
+      email: null,
+      password: null,
+      conPass: null,
+      phone: null,
+      github: null,
+      formErrors: {
+        name: "",
+        regNo: "",
+        email: "",
+        password: "",
+        conPass: "",
+        phone: "",
+        github: "",
+      },
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formValid(this.state)) {
+      console.log(`
+          --SUBMITTING--
+          Name: ${this.state.name}
+          Registration Number: ${this.state.regNo}
+          Email: ${this.state.email}
+          Password: ${this.state.password}
+          Confirm Password: ${this.state.conPass}
+          Phone: ${this.state.phone}
+          Github: ${this.state.github}
+        `);
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+    }
+  };
+
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let formErrors = { ...this.state.formErrors };
+
+    switch (name) {
+      case "name":
+        formErrors.firstName =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      case "regNo":
+        formErrors.userName =
+          value.length < 9 ? "minimum 9 characaters required" : "";
+        break;
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? ""
+          : "invalid email address";
+        break;
+      case "password":
+        formErrors.password =
+          value.length < 8 ? "minimum 8 characaters required" : "";
+        break;
+      case "conPass":
+        /*
+        if (typeof input["password"] !== "undefined" && typeof input["confirm_password"] !== "undefined") {
+          
+        if (input["password"] != input["confirm_password"]) {
+          isValid = false;
+          errors["password"] = "Passwords don't match.";
+        */
+        break;
+      case "phone":
+        formErrors.phone = phoneRegex.test(value) ? "" : "invalid phone number";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
   };
   createAccountClickHandler = () => {
     this.setState({ firstPage: false });
@@ -22,7 +116,7 @@ export class SignUp extends Component {
   formSubmitHandler = (e, a) => {
     const data = JSON.stringify({
       name: this.state.name,
-      regno: this.state.regno,
+      regNo: this.state.regNo,
       email: this.state.email,
       phone: this.state.phone,
       yearofstudy: "1",
@@ -54,94 +148,119 @@ export class SignUp extends Component {
         <form>
           {this.state.firstPage ? (
             <div>
-              <div className='heading'>Sign Up</div>
-              <div className='input-grp'>
+              <div className="heading">Sign Up</div>
+              <div className="input-grp">
                 <label>Name</label>
 
                 <input
-                  className='input'
-                  type='text'
-                  placeholder='Enter your name'
+                  className={formErrors.name.length > 0 ? "error" : null}
+                  type="text"
+                  placeholder="Enter your name"
                   onChange={(event) => {
                     this.inputChangeHandler(event, "name");
                   }}
                 />
+                {formErrors.firstName.length > 0 && (
+                  <span className="errorMessage">{formErrors.name}</span>
+                )}
               </div>
-              <div className='input-grp'>
+              <div className="input-grp">
                 <label>Registration Number</label>
                 <input
-                  className='input'
-                  type='text'
-                  placeholder='Enter Registration number'
+                  className={formErrors.regNo.length > 0 ? "error" : null}
+                  type="text"
+                  placeholder="Enter Registration number"
                   onChange={(event) => {
-                    this.inputChangeHandler(event, "regno");
+                    this.inputChangeHandler(event, "regNo");
                   }}
                 />
+                {formErrors.regNo.length > 0 && (
+                  <span className="errorMessage">{formErrors.regNo}</span>
+                )}
               </div>
-              <div className='input-grp'>
+              <div className="input-grp">
                 <label>Password</label>
                 <input
-                  className='input'
-                  type='password'
-                  placeholder='Enter your password'
+                  className={formErrors.password.length > 0 ? "error" : null}
+                  type="password"
+                  placeholder="Enter your password"
                   onChange={(event) => {
                     this.inputChangeHandler(event, "password");
                   }}
                 />
+                {formErrors.password.length > 0 && (
+                  <span className="errorMessage">{formErrors.password}</span>
+                )}
               </div>
-              <div className='input-grp'>
+              <div className="input-grp">
                 <label>Confirm password</label>
                 <input
-                  className='input'
-                  type='password'
-                  placeholder='Confirm password'
+                  className={formErrors.conPass.length > 0 ? "error" : null}
+                  type="password"
+                  placeholder="Confirm password"
+                  onChange={(event) => {
+                    this.inputChangeHandler(event, "conPass");
+                  }}
                 />
+                {formErrors.conPass.length > 0 && (
+                  <span className="errorMessage">{formErrors.conPass}</span>
+                )}
               </div>
-              <div className='sub-btn' onClick={this.createAccountClickHandler}>
+              <div className="sub-btn" onClick={this.createAccountClickHandler}>
                 Create account
               </div>
             </div>
           ) : (
             <div>
-              <div className='heading'>Sign Up</div>
-              <div className='input-grp'>
+              <div className="heading">Sign Up</div>
+              <div className="input-grp">
                 <label>Phone</label>
                 <input
-                  className='input'
-                  type='text'
-                  placeholder='Enter your phone no.'
+                  className={formErrors.phone.length > 0 ? "error" : null}
+                  type="text"
+                  placeholder="Enter your phone no."
                   onChange={(event) => {
                     this.inputChangeHandler(event, "phone");
                   }}
                 />
+                {formErrors.phone.length > 0 && (
+                  <span className="errorMessage">{formErrors.phone}</span>
+                )}
               </div>
-              <div className='input-grp'>
+              <div className="input-grp">
                 <label>VIT-Email</label>
                 <input
-                  className='input'
-                  type='text'
-                  placeholder='Enter your VIT Email'
+                  className={formErrors.email.length > 0 ? "error" : null}
+                  type="text"
+                  placeholder="Enter your VIT Email"
                   onChange={(event) => {
                     this.inputChangeHandler(event, "email");
                   }}
                 />
+                {formErrors.email.length > 0 && (
+                  <span className="errorMessage">{formErrors.email}</span>
+                )}
               </div>
-              <div className='input-grp'>
+              <div className="input-grp">
                 <label>Github</label>
                 <input
-                  className='input'
-                  type='text'
-                  placeholder='Enter your github handle'
+                  className={formErrors.github.length > 0 ? "error" : null}
+                  type="text"
+                  placeholder="Enter your github handle"
                   onChange={(event) => {
                     this.inputChangeHandler(event, "github");
                   }}
                 />
+                {formErrors.github.length > 0 && (
+                  <span className="errorMessage">{formErrors.github}</span>
+                )}
               </div>
               <div
-                className='sub-btn'
+                className="sub-btn"
                 onClick={(event) => {
                   this.formSubmitHandler(event, this.props);
-                }}>
+                }}
+              >
                 Sign Up
               </div>
             </div>
@@ -151,5 +270,4 @@ export class SignUp extends Component {
     );
   }
 }
-
 export default SignUp;
